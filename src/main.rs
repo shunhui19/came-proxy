c187eab398f7a671a41a2d9b11d4680d9060025e
frank <shunhui29@163.com>
2024-01-14 20:59:31 +0800

    add new file to rust module system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+pub mod common;
+pub mod config;
+
+use common::log::*;
+use tracing::info;
+
 fn main() {
     println!("Hello, world!");
+
+    Logger::init(Some("app".to_string()));
+    info!("came proxy starting...");
 }
